f1a889e48453d09925bd358b81c7107b21595bb7
Ankan <ankanb560@gmail.com>
2026-03-22 19:33:56 +0530

    Stack DSA

---
+++ Stack/stack.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack[-1]
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+
+my_stack = Stack()
+
+my_stack.push(1)
+my_stack.push(2)
+my_stack.push(3)
+
+print("Stack: ", my_stack.stack)
+print("Pop: ", my_stack.pop())
+print("Stack after Pop: ", my_stack.stack)
+print("Peek: ", my_stack.peek())
+print("isEmpty: ", my_stack.isEmpty())
+print("Size: ", my_stack.size())
